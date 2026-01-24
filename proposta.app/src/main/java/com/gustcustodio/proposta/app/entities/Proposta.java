ec1680c8076a3777ca661316646d8412f1a54801
gustcustodio <gustcustodio@gmail.com>
2026-01-24 06:35:39 -0300

    feat: mapeando as entidades

---
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Proposta.java
@@ -0,0 +1,27 @@
+package com.gustcustodio.proposta.app.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_proposta")
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovado;
+
+    private boolean integrada;
+
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+}
---
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Usuario.java
@@ -0,0 +1,26 @@
+package com.gustcustodio.proposta.app.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private Double renda;
+
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+
+}
