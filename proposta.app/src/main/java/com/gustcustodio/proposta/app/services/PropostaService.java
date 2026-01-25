1f41272153c90116136c22278a46f77915d617c4
gustcustodio <gustcustodio@gmail.com>
2026-01-25 13:11:52 -0300

    feat: cria PropostaService com primeiro endpoint

---
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/services/PropostaService.java
@@ -0,0 +1,26 @@
+package com.gustcustodio.proposta.app.services;
+
+import com.gustcustodio.proposta.app.dtos.PropostaRequestDTO;
+import com.gustcustodio.proposta.app.dtos.PropostaResponseDTO;
+import com.gustcustodio.proposta.app.entities.Proposta;
+import com.gustcustodio.proposta.app.mapper.PropostaMapper;
+import com.gustcustodio.proposta.app.repositories.PropostaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PropostaService {
+
+    private final PropostaMapper propostaMapper;
+    private final PropostaRepository propostaRepository;
+
+    @Transactional
+    public PropostaResponseDTO criar(PropostaRequestDTO requestDTO) {
+        Proposta entity = propostaMapper.convertDtoToEntity(requestDTO);
+        propostaRepository.save(entity);
+        return propostaMapper.convertEntityToDto(entity);
+    }
+
+}
