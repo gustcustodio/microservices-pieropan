6950bece8eb2c0141d3ceea2fac0c4e2651d896f
gustcustodio <gustcustodio@gmail.com>
2026-01-25 10:02:59 -0300

    feat: adiciona annotations lombok

--- proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Proposta.java
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Proposta.java
@@ -1,7 +1,21 @@
 package com.gustcustodio.proposta.app.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_proposta")
 public class Proposta {
@@ -14,7 +28,7 @@ public class Proposta {
 
     private int prazoPagamento;
 
-    private Boolean aprovado;
+    private Boolean aprovada;
 
     private boolean integrada;
 
--- proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Usuario.java
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/entities/Usuario.java
@@ -1,7 +1,20 @@
 package com.gustcustodio.proposta.app.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
