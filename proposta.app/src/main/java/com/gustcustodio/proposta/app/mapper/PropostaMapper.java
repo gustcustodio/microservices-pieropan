934d2134000df1f95d5978a6e37a95961fa0bb9a
gustcustodio <gustcustodio@gmail.com>
2026-01-25 13:12:17 -0300

    feat: cria PropostaMapper

---
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/mapper/PropostaMapper.java
@@ -0,0 +1,30 @@
+package com.gustcustodio.proposta.app.mapper;
+
+import com.gustcustodio.proposta.app.dtos.PropostaRequestDTO;
+import com.gustcustodio.proposta.app.dtos.PropostaResponseDTO;
+import com.gustcustodio.proposta.app.entities.Proposta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PropostaMapper {
+
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.renda", source = "renda")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aprovada", ignore = true)
+    @Mapping(target = "integrada", ignore = true)
+    @Mapping(target = "observacao", ignore = true)
+    Proposta convertDtoToEntity(PropostaRequestDTO propostaRequestDTO);
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "renda", source = "usuario.renda")
+    PropostaResponseDTO convertEntityToDto(Proposta proposta);
+
+}
