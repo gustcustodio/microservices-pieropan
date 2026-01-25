b33fd74d2985dacd93e5b7cb059d4d4dbc90d28c
gustcustodio <gustcustodio@gmail.com>
2026-01-25 13:10:30 -0300

    feat: cria PropostaResponseDTO

---
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/dtos/PropostaResponseDTO.java
@@ -0,0 +1,25 @@
+package com.gustcustodio.proposta.app.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PropostaResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private Double renda;
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private String observacao;
+
+}
