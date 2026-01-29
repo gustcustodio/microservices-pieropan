e8e5fddaccd5f385f8c0ab426660613c74b93d4c
gustcustodio <gustcustodio@gmail.com>
2026-01-29 06:32:49 -0300

    feat: cria exchange e bind de proposta-pendente

--- proposta.app/src/main/java/com/gustcustodio/proposta/app/configs/RabbitMQConfiguration.java
+++ proposta.app/src/main/java/com/gustcustodio/proposta/app/configs/RabbitMQConfiguration.java
@@ -1,7 +1,6 @@
 package com.gustcustodio.proposta.app.configs;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -40,6 +39,23 @@ public class RabbitMQConfiguration {
     @Bean
     public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
         return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente() {
+        return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsAnaliseCredito() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseCredito()).
+                to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao()).
+                to(criarFanoutExchangePropostaPendente());
     }
 
 }
