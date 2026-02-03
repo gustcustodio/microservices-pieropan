38fdeee05dbf1f89ec5442080d057190fa609453
gustcustodio <gustcustodio@gmail.com>
2026-02-03 06:17:29 -0300

    feat: cria entidade PropostaPendenteListener

---
+++ analise-credito/src/main/java/com/gustcustodio/analise_credito/listener/PropostaPendenteListener.java
@@ -0,0 +1,14 @@
+package com.gustcustodio.analise_credito.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaPendenteListener {
+
+    @RabbitListener(queues = "{rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente() {
+
+    }
+
+}
