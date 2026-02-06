dac4928c178bbf207866146c2dd4f64a81c5c3b5
gustcustodio <gustcustodio@gmail.com>
2026-02-06 06:48:53 -0300

    feat: cria MensagemConstante

---
+++ analise-credito/src/main/java/com/gustcustodio/analise_credito/constants/MensagemConstante.java
@@ -0,0 +1,8 @@
+package com.gustcustodio.analise_credito.constants;
+
+public class MensagemConstante {
+
+    public static final String CLIENTE_NEGATIVADO = "Operação não permitida. Cliente %s com nome negativado.";
+    public static final String PONTUACAO_SERASA_BAIXA = "Operação não permitida. Cliente %s com score abaixo de 200 pontos.";
+
+}
