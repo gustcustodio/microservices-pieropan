f24abd3b7aec1f40d67133dd4dffd3cbf6ac8ab2
gustcustodio <gustcustodio@gmail.com>
2026-02-06 06:48:17 -0300

    feat: implementa lógica AnaliseCreditoService

--- analise-credito/src/main/java/com/gustcustodio/analise_credito/services/AnaliseCreditoService.java
+++ analise-credito/src/main/java/com/gustcustodio/analise_credito/services/AnaliseCreditoService.java
@@ -1,7 +1,9 @@
 package com.gustcustodio.analise_credito.services;
 
 import com.gustcustodio.analise_credito.domain.Proposta;
+import com.gustcustodio.analise_credito.exceptions.StrategyException;
 import com.gustcustodio.analise_credito.services.strategy.CalculoPonto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +12,25 @@ import java.util.List;
 public class AnaliseCreditoService {
 
     private List<CalculoPonto> calculoPontoList;
+    private NotificacaoRabbitService notificacaoRabbitService;
 
-    public AnaliseCreditoService(List<CalculoPonto> calculoPontoList) {
+    @Value("${rabbitmq.proposta-concluida.exchange}")
+    private String exchangePropostaConcluida;
+
+    public AnaliseCreditoService(List<CalculoPonto> calculoPontoList, NotificacaoRabbitService notificacaoRabbitService) {
         this.calculoPontoList = calculoPontoList;
+        this.notificacaoRabbitService = notificacaoRabbitService;
     }
 
     public void analisar(Proposta proposta) {
-        calculoPontoList.stream().mapToInt(impl -> impl.calcular(proposta)).sum();
+        try {
+            int pontos = calculoPontoList.stream().mapToInt(impl -> impl.calcular(proposta)).sum();
+            proposta.setAprovada(pontos > 350);
+        } catch (StrategyException exception) {
+            proposta.setAprovada(false);
+            proposta.setObservacao(exception.getMessage());
+        }
+        notificacaoRabbitService.notificar(exchangePropostaConcluida, proposta);
     }
 
 }
