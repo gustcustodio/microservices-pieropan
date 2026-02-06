581ba99b1282fe26f490d322bfffa3aedb9a3250
gustcustodio <gustcustodio@gmail.com>
2026-02-06 06:52:31 -0300

    feat: implementa lógica PropostaPendenteListener

--- analise-credito/src/main/java/com/gustcustodio/analise_credito/listener/PropostaPendenteListener.java
+++ analise-credito/src/main/java/com/gustcustodio/analise_credito/listener/PropostaPendenteListener.java
@@ -1,14 +1,22 @@
 package com.gustcustodio.analise_credito.listener;
 
+import com.gustcustodio.analise_credito.domain.Proposta;
+import com.gustcustodio.analise_credito.services.AnaliseCreditoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
-    @RabbitListener(queues = "{rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente() {
+    private final AnaliseCreditoService analiseCreditoService;
 
+    public PropostaPendenteListener(AnaliseCreditoService analiseCreditoService) {
+        this.analiseCreditoService = analiseCreditoService;
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta) {
+        analiseCreditoService.analisar(proposta);
     }
 
 }
