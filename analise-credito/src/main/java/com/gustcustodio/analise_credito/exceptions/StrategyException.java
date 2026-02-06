5c8f8a2cd2e1db76831c4010867897c643333cdf
gustcustodio <gustcustodio@gmail.com>
2026-02-06 06:48:30 -0300

    feat: cria StrategyException

---
+++ analise-credito/src/main/java/com/gustcustodio/analise_credito/exceptions/StrategyException.java
@@ -0,0 +1,9 @@
+package com.gustcustodio.analise_credito.exceptions;
+
+public class StrategyException extends RuntimeException {
+
+    public StrategyException(String mensagem) {
+        super(mensagem);
+    }
+
+}
